a9dfd4181eec1ea127eab62438a4720861274c23
Pereira De Paiva <fernando.pereira@transfesa.com>
2024-01-16 17:49:27 +0100

    Funcion Guards

--- src/app/auth/pages/login-pages/login-pages.component.ts
+++ src/app/auth/pages/login-pages/login-pages.component.ts
@@ -1,8 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
+
 import { AuthService } from '../../services/auth.service';
 
-import Swal from 'sweetalert2'
 
 
 @Component({
@@ -14,12 +17,13 @@ import Swal from 'sweetalert2'
 })
 export class LoginPagesComponent {
 
-  private fb = inject( FormBuilder );
-  private authService= inject( AuthService );
+  private fb          = inject( FormBuilder );
+  private authService = inject( AuthService );
+  private router      = inject( Router );
 
   public myForm: FormGroup = this.fb.group({
     email: ['fernando@google.com', [ Validators.required, Validators.email ]],
-    password: ['1234567', [ Validators.required, Validators.minLength(6) ]],
+    password: ['123456', [ Validators.required, Validators.minLength(6) ]],
 
   });
 
@@ -27,7 +31,7 @@ export class LoginPagesComponent {
     const { email, password } = this.myForm.value;
     this.authService.login( email, password )
     .subscribe( {
-      next: () => console.log('Todo bien!'),
+      next: () => this.router.navigate(['/dashboard']),
       error: (message) => {
         Swal.fire( 'Error', message, 'error' )
       }
--- src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
+  standalone: true,
+  imports: [ CommonModule ],
   templateUrl: './dashboard-layout.component.html',
   styleUrls: ['./dashboard-layout.component.css']
 })
 export class DashboardLayoutComponent {
+
+  private authService = inject( AuthService );
+
+  public user = computed( () => this.authService.currentUser() );
+
+  // get user() {
+  //   return this.authService.currentUser();
+  // }
 
 }
