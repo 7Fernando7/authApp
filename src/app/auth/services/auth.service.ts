609a403861a4cf95e859fa4cddc28af53f9af36c
Pereira De Paiva <fernando.pereira@transfesa.com>
2024-01-16 15:52:20 +0100

    Tipos de interfaces

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,5 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import { environment } from '../../../enviroments/environments';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+import { User, AuthStatus } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +12,16 @@ export class AuthService {
 
   private readonly baseUrl: string = environment.baseUrl;
 
+  private http = inject( HttpClient );
+
+  private _currentUser = signal<User|null>(null);
+  private _authStatus = signal<AuthStatus>( AuthStatus.checking);
+
   constructor() { }
+
+  login( email: string, password: string): Observable<boolean> {
+
+
+    return of(true);
+  }
 }
